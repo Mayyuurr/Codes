f6785f4a62cf04c4e000c4e3e48537e17801bdbc
May9988 <mayurrathodavni@gmail.com>
2022-10-21 16:05:46 +0530

    JavaScript SOP3(JS)

--- 12th SOP's/JavaScript SOP3(JS).js
+++ 12th SOP's/JavaScript SOP3(JS).js
@@ -6,7 +6,7 @@ function getVowels() {
       if (string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u") {
         vowelsCount += 1;
       }
-    }
+    } 
     //document.write("Total Vowels : "+vowelsCount);
    document.getElementById('demo').innerHTML = "Total no. of Vowels in string are: "+vowelsCount;
     return vowelsCount;
